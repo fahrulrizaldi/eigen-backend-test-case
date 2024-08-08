1ce25b8e47e78130de6a3e78aba49b7fee898f9f
fahrulrizaldi <fahrulrizaldi99@gmail.com>
2024-08-08 16:16:09 +0700

    connect to database

--- members.php
+++ members.php
@@ -4,40 +4,41 @@
     <!-- End Header -->
 
     <!-- Main Navbar -->
-<div class="contrainer">
-    <div>
+  <div class="contrainer">
+    <div class="headerdata">
       <h2> Data Members </h2>
+      <a href="backend/buku/tambah.php" class="btn btn-primary" role="button">Tambah Data</a>
     <div>
-    <button type="button" class="btn btn-primary">Tambah Data</button>
+    
     <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-          </tr>
-        </tbody>
-    </table>
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 </div>
     <!-- End Navbar -->
 
